90fd983e404e0c06ed7f390e43d34fb33ea70d3e
Alessandro Davi <Alessandro.dfm@hotmail.com>
2024-04-02 21:50:08 -0300

    att3

---
+++ database/seeders/Userseeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class Userseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Alessandro Davi',
+            'email' => 'alessandro.dfm@hotmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'João Procopio',
+            'email' => 'joao.procopio@hotmail.com',
+            'password' => Hash::make('12345679'),
+        ]);
+    }
+}
